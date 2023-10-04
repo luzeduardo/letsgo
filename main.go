599916d74bfc81219e07835c86a9d5e3a7b6932e
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-04 18:21:06 +0100

    adding basic routes

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("home!"))
 }
 
+func sniView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Sni show"))
+}
+
+func sniCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("sni create"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/sni/view", sniView)
+	mux.HandleFunc("/sni/create", sniCreate)
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
