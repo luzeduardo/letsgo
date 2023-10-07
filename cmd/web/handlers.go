affe9b7bb2f210d1b9b4306f63545d087ae7cba8
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-07 08:36:09 +0100

    fix: execute template instead of a parsed template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,8 +16,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	files := []string{
 		"./ui/html/base.tmpl",
-		"./ui/html/pages/home.tmpl",
 		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
@@ -26,7 +26,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
