d158d1181180170f75de1ff5a63cfff430c88af8
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-07 08:15:43 +0100

    feat: serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/sni/view", sniView)
 	mux.HandleFunc("/sni/create", sniCreate)
