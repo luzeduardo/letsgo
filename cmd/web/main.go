ae68c8dddd28a5fac96d2263828d070c327931e0
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-08 23:26:51 +0100

    feat: setting up db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type config struct {
@@ -18,8 +21,21 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn) // initi the pool for future use
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil { //verify if is setup correctly creating a connection
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	var cfg config
+	dsn := flag.String("dsn", "", "MySQl data source name")
+
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
@@ -27,6 +43,12 @@ func main() {
 	//log.New(destination, prefix, additional info)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Llongfile)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -40,6 +62,7 @@ func main() {
 		ErrorLog: errorLog,
 		Handler:  app.routes(cfg),
 	}
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
