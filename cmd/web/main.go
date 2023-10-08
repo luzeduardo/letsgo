963a6ee2a824b7f32a9f0d87116f7c5b14d38120
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-08 08:18:32 +0100

    refactor: parsing cmd flag values into memory address of pre-existing variables

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,18 +6,26 @@ import (
 	"net/http"
 )
 
+type config struct {
+	addr string
+	staticDir string
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/sni/view", sniView)
 	mux.HandleFunc("/sni/create", sniCreate)
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	log.Printf("Starting server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
