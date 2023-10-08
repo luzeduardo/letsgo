a7a701f0de3485beb96d36a1c2347aa9f16ed271
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-08 20:45:39 +0100

    feat: centralizing errors with the dependecy injection and consuming it in the handler from the helper

---
+++ cmd/web/helpers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// writes error message and stack trace to errorLog and return a http 500
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Print(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// sends a specfic status code and description to the user
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
