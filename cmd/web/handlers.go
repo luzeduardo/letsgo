abb397da5fc8ec17e6376861365bcfd2fdf50365
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-10 20:17:36 +0100

    feat: using the model in the handler to insert data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -50,6 +50,16 @@ func (app *application) sniCreate(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"name":"JOhn Doe"`))
+
+	title := "To dos"
+	content := "Go for devs\nDDD\nGo for dummies"
+	expires := 10
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
 }
