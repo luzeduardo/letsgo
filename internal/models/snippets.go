6a40be586056f8b765228058e567cc3787d4fe94
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-11 20:04:43 +0100

    feat: retriving multiple rows

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -20,6 +20,42 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	//ensures sql.Rows results is always properly closed before the Latest() method returns
+	// must become after the error check from Query() method
+	// if Query returns an error, it'll result in a panic trying to close a
+	// nil resultset
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	//iterate through the rows in the resultset
+	//it closes automatically and closes itself and frees-up the DB connection
+	for rows.Next() {
+		s := &Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+	//after rows.Next ends, checks if some error ocurred during the iteration
+	// do not assume that the iteration completed successfully
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
+}
+
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
