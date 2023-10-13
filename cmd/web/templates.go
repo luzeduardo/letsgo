86dcb62fc2b6b257c400a9ba52d79a9353427d5a
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-13 12:17:23 +0100

    feat: using struct to hold and handle the dynamic data to the template

---
+++ cmd/web/templates.go
@@ -0,0 +1,8 @@
+package main
+
+import "poc.eduardo-luz.eu/internal/models"
+
+// acts as a holding structure for dynamic data that will be available to the templates
+type templateData struct {
+	Snippet *models.Snippet
+}
