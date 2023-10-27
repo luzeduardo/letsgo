b16178b9c84bfcd0ad3de0a8a3d5b061bed47151
Eduardo Luz <luz.eduardo@gmail.com>
2023-10-27 16:24:46 +0100

    feat: adding new error types

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,10 @@ import "errors"
 
 // this error models helps encapsulate the model completely.
 // the application is not concerned with the underlying datastore or datastore-specific errors
-var ErrNoRecord = errors.New("models: no mathcing record found")
+var (
+	ErrNoRecord = errors.New("models: no mathcing record found")
+
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	ErrDuplicatedEmail = errors.New("models: dupplicate email")
+)
